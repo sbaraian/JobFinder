2191b4193a56bc78354e669f68298dd0507f9101
sbaraian <sbaraian@hotmail.com>
2015-01-07 17:35:16 +0000

    posting job front end

--- jobs-data.js
+++ jobs-data.js
@@ -7,25 +7,28 @@ var findJobs=function(query){
     return Promise.cast(Job.find(query).exec())
 }
 
-var jobs=[
-        {title:'Cook',description:'You will be making bagels'},
-        {title:'Waiter',description:'You will be putting food on peoples table'},
-        {title:'Programmer',description:'You will be mindlessly type'},
-        {title:'Axe Maker',description:'We need many axes made ... so many...'}
-];
-
 var createJob=Promise.promisify(Job.create, Job);
 
+//exports
 exports.connectDb=Promise.promisify(mongoose.connect,mongoose);
+
 exports.findJobs = findJobs;
 
+exports.saveJob=createJob;
 
 exports.seedJobs=function(){
     return findJobs({}).then(function(collection){
         if(collection.length===0){
-            return Promise.map(jobs,function(job){
+            return Promise.map(seedJobs,function(job){
                 return createJob(job);
             })
         }
     });
 }
+
+var seedJobs=[
+        {title:'Cook',description:'You will be making bagels'},
+        {title:'Waiter',description:'You will be putting food on peoples table'},
+        {title:'Programmer',description:'You will be mindlessly type'},
+        {title:'Axe Maker',description:'We need many axes made ... so many...'}
+];
---
+++ test/server/jobs-data-spec.js
@@ -0,0 +1,72 @@
+var expect=require("chai").expect;
+var mongoose=require('mongoose');
+var Promise=require("bluebird");
+var jobsData=require("../../jobs-data.js");
+
+function resetJobs(callback){
+    return new Promise(function(resolve, reject){
+        mongoose.connection.collections['jobs'].drop(resolve, reject);
+    });
+}
+
+
+describe("db get jobs", function(){
+    
+    var jobs;
+    
+    before(function(done){
+        jobsData.connectDb('mongodb://localhost/jobfinder')
+        .then(resetJobs)
+        .then(jobsData.seedJobs)
+        .then(jobsData.findJobs)
+        .then(function(collection){
+            jobs=collection;
+            done();
+        });        
+    });
+    
+    after(function(){
+        mongoose.connection.close();
+    })
+    
+    it("should never be empty since jobs are seeded", function(){
+        expect(jobs.length).to.be.at.least(1);
+    });
+    
+    it("should have a job with a title", function(){
+        expect(jobs[0].title).to.not.be.empty;
+    });
+    
+    it("should have a job with a description", function(){
+        expect(jobs[0].description).to.not.be.empty;
+    });
+});
+
+describe("db save jobs", function(){
+    var job={title:'Cook', description:'You will be making bagels'};
+    var jobs;
+    
+    function saveTestJob(){
+        return jobs.saveJob(job);
+    }
+    
+    before(function(done){
+        jobsData.connectDb('mongodb://localhost/jobfinder')
+        .then(resetJobs)
+        .then(function(){return jobsData.saveJob(job);})
+        .then(jobsData.findJobs)
+        .then(function setJobs(collection){
+            jobs=collection;
+            done();
+        });
+    })
+    
+    after(function(){
+        mongoose.connection.close();
+    })
+    
+    it("should have one job after saving one job", function(){
+        expect(jobs).to.have.length(1);
+    })
+        
+})
